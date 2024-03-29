72f163399ef0e0dfcfe9575ac29ca3a71ef52d94
Vishalra-i <vishalrai0392@gmail.com>
2024-03-29 11:07:14 +0530

    fix some issue in dark mode button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import Button from "@/components/Button";
 
 export default function Home() {
+  
 
   const darkMode = () => {
     const html = document.getElementsByTagName('html')[0];
@@ -15,7 +16,9 @@ export default function Home() {
   return (
     <>
       <div className={`bg-[#F7F7F8]  dark:bg-[#292B32] dark:text-white  w-full min-h-screen max-[700px]:px-2 px-10 py-5`}>
-        <Button darkMode={darkMode}/>
+         <div className=" px-2 relative py-2 w-full justify-between">
+           <Button className="border-red-100 absolute top-[-5]  right-0 " darkMode={darkMode}/>
+        </div>
         <Sports />
         <Collection />
       </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-interface Props{
-    darkMode:()=>void
+import React, { useState } from 'react';
+
+interface Props {
+  darkMode: () => void,
+  className : string
 }
-function Button(props:Props) {
-    const {darkMode} = props
+
+function Button(props: Props) {
+  const { darkMode , className = ''} = props;
+  const [buttonText, setButtonText] = useState("🌙"); // Default button text
+
+  const handleDarkModeToggle = () => {
+    darkMode(); 
+    setButtonText(prevText => prevText ===  "🌙" ? "🌞" : "🌙");
+  };
+
   return (
     <div className='m-0 p-0'>
-            <button onClick={darkMode} className="absolute top-[-6px] right-24 bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-gray-200 py-2 px-4 rounded-md mt-4">
-          Toggle Dark Mode
+  
+        <button onClick={handleDarkModeToggle} className={` ${className} rounded-full text-2xl  bg-gray-500 dark:bg-gray-700 text-gray-800 dark:text-gray-200 px-4 py-2 mt-4 `}>
+          {buttonText}
         </button>
+     
     </div>
   )
 }
 
-export default Button
+export default Button;
